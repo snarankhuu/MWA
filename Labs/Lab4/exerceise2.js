473b1065cff18debe3a17f1bef3606b1e437cc37
Surenkhuu Narankhuu <snarankhuu@mum.edu>
2018-11-30 00:58:01 -0600

    checkSystemObservable

--- Labs/Lab4/exerceise2.js
+++ Labs/Lab4/exerceise2.js
@@ -1,6 +1,29 @@
 {
-    const os = require('os');
+    const os = require('os')
+    const { Observable  } = require('rxjs')
 
-    console.log(os.totalmem() / (1024 * 1024 ), os.cpus().length)
+    const checkSystem = () => {
+        console.log('Checking your system ...');
+        if (os.totalmem() / (1024 * 1024 * 1024) < 4) console.log('This app needs at least 4GB of RAM')
+        else if (os.cpus().length < 2) console.log('Processor is not supported')
+        else console.log('System is checked successfully.')
+    }
+
+    checkSystem();
+    console.log('');
+
+    console.log('Observable way');
+    var checkSystemObservable = Observable.create((observer) => {
+        observer.next('Checking your system ...');
+        if (os.totalmem() / (1024 * 1024 * 1024) < 4) observer.next('This app needs at least 4GB of RAM')
+        else if (os.cpus().length < 2) observer.next('Processor is not supported')
+        else observer.complete()
+    })
+ 
+    checkSystemObservable.subscribe({
+        next: (x) => { console.log(x) },
+        undefined,
+        complete: () => console.log('System is checked successfully.'),
+    });
 
 }
