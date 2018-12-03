2226d34ab58b456885c13545266d5ef9e773b84a
Surenkhuu Narankhuu <snarankhuu@mum.edu>
2018-12-02 23:53:18 -0600

    lab5

--- Labs/Lab5/index.js
+++ Labs/Lab5/index.js
@@ -1,45 +1,67 @@
 {
     const axios = require('axios')
     const express = require('express')
+    const { Observable } = require('rxjs')
     const app = express()
-
-    const promise = () => {
-            return axios.get('https://randomuser.me/api/?results=1')
-    }
-
-    function getDataPromise() {
-        return promise().then(result => {
-            //   console.log(result.data)
-            return result.data
-        })
-            .catch(err => {
-                //       console.error(err);
-                throw err
-            })
-    }
-    async function getDataA() {
-        try {
-            return await promise()
-        } catch (error) {
-            
-        }
-      
-    }
-
-    app.get('/', (req, res) => {
-        res.status(200).set({ "Content-type": "text/html" }).send("Welcome").end()
-    })
-    app.get('/users', (req, res) => {
-        let data = getDataA()
-        console.log(data)
-     //   console.log(getDataPromise())
-        res.status(200).set({ 'Content-Type': 'application/json' }).send(data).end()
-    })
 
     app.set('x-powered-by', false)
     app.set('trust proxy', true)
     app.set('strict routing', true)
     app.set('case sensitive routing', true)
+
+    function getDataObservable(res) {
+        Observable.create(observer => {
+            axios.get('https://randomuser.me/api/?results=10')
+                .then(({ data }) => {
+                    observer.next(data)
+                    observer.complete()
+                })
+                .catch(e => observer.error(e))
+
+        }).subscribe(
+            x => {
+                res.write(render(x, 'Observable'))
+            },
+            e => { console.log(e) },
+            () => { console.log('Observable Complete') }
+        )
+    }
+
+    function getDataPromise(res) {
+        axios.get('https://randomuser.me/api/?results=10').then(({ data }) => {
+            res.write(render(data, 'promise'))
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    async function getDataAsync(res) {
+        try {
+            const { data } = await axios.get('https://randomuser.me/api/?results=10')
+            res.write(render(data, 'async/await'))
+
+        } catch (error) {
+            console.log(err)
+        }
+    }
+
+    function render(users, name) {
+        const divs = users.results.map(user => `<div>${user.name.first} ${user.name.last}, ${user.email}</div>`).join(" ")
+        return `<div> <strong>${name}</strong></div> ${divs}`
+    }
+
+    app.get('/users', (req, res) => {
+        res.set({
+            "Cache-Control": "private, max-age: 86400"
+            // , "Content-type": "text/html"
+        })
+        getDataAsync(res)
+        getDataPromise(res)
+        getDataObservable(res)
+        setTimeout(()=> res.end(), 3000)
+    })
+
+
 
     app.listen(4000, () => {
         console.log('The server running localhost:4000')
